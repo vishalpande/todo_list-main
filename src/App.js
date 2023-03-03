3e868a1f6e4f9a0eea2587f4eaeadf9b63d13f6f
vishalpande <pandevishal40@gmail.com>
2023-03-03 09:54:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import ToDoLists from "./ToDoLists";
+
+const App = () =>
+{
+    const [inputList, setInputList] = useState("");
+    const [Items, setItems] = useState([]);
+
+    const itemEvent=(event)=>{
+        setInputList(event.target.value);
+    };
+
+    const listOfItems = () =>
+    {
+       setItems((oldItems) =>
+       {
+          return [...oldItems, inputList];
+       });
+       setInputList("");
+    };
+
+    const deleteItems = (id) =>{
+         console.log("deleted");  
+         
+         setItems((oldItems) => {
+            return oldItems.filter((arrElem, index)=> {
+                return index !== id;
+            });
+         });
+    };
+
+
+
+    return(
+        <>
+            <div className="main_div">
+               <div className="center_div">
+                    <br />
+                    <h1 className="Cursor" onClick={() =>
+          (window.location =
+            'https://github.com/jyoti369/todo_list')
+        }>ToDo List</h1>
+                    <br />
+                    <input type="text" placeholder="Add an Item" 
+                        value={inputList}
+                        onChange={itemEvent}
+                    />
+                    <button onClick={listOfItems}> + </button>
+                    
+                     <ol>
+                         {/* <li>{inputList}</li> */}
+                         {Items.map((itemval, index) =>
+                         {
+                           return <ToDoLists key={index}
+                           id={index}
+                           text={itemval} 
+                           onSelect={deleteItems}    
+                           />;
+                         })}
+                     </ol>
+               </div>
+            </div>
+        </>
+);
+};
+
+export default App;
